135053e1c5acb5bc642000aeef87059ea28e4a3b
interactive-cache-8375 <randomblue1@yahoo.com>
2019-10-01 03:10:19 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,25 @@
+
+
 def square_array(array)
-  # your code here
+  # nothing 
 end
+
+
+
+
+
+def square_array(array)
+  new_array = []
+  counter = 0 
+  
+  while counter < array.length do 
+  new_array << array[counter]  ** 2
+  counter += 1 
+  
+end
+  
+  return new_array
+end
+
+# new_array.each 
+# while
